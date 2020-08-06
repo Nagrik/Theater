b7df91c8937f2cbe934a7b82f1ee7aa9e8db9d3e
RomanNahryshko <nagrishkoo@gmail.com>
2020-08-06 23:28:31 +0300

    First commit

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/Arrow-right.png" alt="prev arrow"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/Arrow-left.png" alt="next arrow"></button>'
+    });
+    $('.Staging__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        arrows: false,
+    });
+});
+
+ 
